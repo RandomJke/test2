5ff6a386a2d3ef477060d936a8af5d347f0aab71
RandomJke <md_St1m@mail.rugit config --list git config --listgit config --global user.email md_St1m@mail.ru>
2023-01-04 19:16:57 +0600

    second

---
+++ src/main/java/com/example/myspringproject/config/KafkaConsumerConfig.java
@@ -0,0 +1,2 @@
+package com.example.myspringproject.config;public class KafkaConsumerConfig {
+}
---
+++ src/main/java/com/example/myspringproject/controller/KafkaExampleController.java
@@ -0,0 +1,2 @@
+package com.example.myspringproject.controller;public class KafkaExampleController {
+}
